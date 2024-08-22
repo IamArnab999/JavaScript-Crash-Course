682f2cc4881c6c6dbe9d088cd0c3e4c0c6b7a2a3
IamArnab999 <arnabkantdasi2004@gmail.com>
2024-08-22 17:26:55 +0530

    commit ho geya

--- scopes.js
+++ scopes.js
@@ -1,9 +1,51 @@
 const a = 24
 
-if(true){
+if (true) {
     let a = 200
     // let b = 3004
     // console.log("Inner a:", a)
 }
 
-console.log(a)
+// console.log(a)
+
+function one() {
+    const username = "hitesh"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+
+one()
+
+
+/*
+if(true){
+    const username = "Arnab"
+    if (username === "Enable yourself"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+*/
+
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1
+}
+
+
+// second type:
+
+const addTwo = function (num) {
+    return num + 2
+}
+addTwo(5)
