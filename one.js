32d6585fa77903a1cc93611c2f3e0c8913f4428a
IamArnab999 <arnabkantdasi2004@gmail.com>
2024-08-23 22:32:14 +0530

    initial commit

---
+++ one.js
@@ -0,0 +1,42 @@
+/*
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    default:
+        break;
+}
+*/
+
+
+// truthy values
+
+const userEmail = []
+if(userEmail){
+    console.log("Got user email");
+}
+else{
+    console.log("Don't have user email");
+}
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", undefined, null, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
